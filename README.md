41a371492e8c2fd984b32b4ec6be7bd89b96512e
Daan Mooij <daanmooij@gmail.com>
2019-05-19 16:53:52 +0200

    setup composer phpunit travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/daanmooij/domino.svg?branch=master)](https://travis-ci.org/daanmooij/domino)
+
 # domino
 library to play domino
---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DaanMooij\Domino;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
